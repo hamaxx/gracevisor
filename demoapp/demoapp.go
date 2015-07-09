f865d2ae737f6099e91e32b21d21fc15da4b2675
Domen Ipavec <domen@ipavec.net>
2015-07-09 17:01:01 +0200

    Added envirnoment variable for PORT.
    Signed-off-by: Domen Ipavec <domen@ipavec.net>

--- demoapp/demoapp.go
+++ demoapp/demoapp.go
@@ -6,24 +6,33 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/user"
+	"strconv"
 )
 
-var port = flag.Int("port", 8080, "port")
+var portFlag = flag.Int("port", 8080, "port")
 
 func main() {
-	flag.Parse()
-	fmt.Println("Listening on port", *port)
-	
+	port, _ := strconv.Atoi(os.Getenv("PORT"))
+	if port == 0 {
+		flag.Parse()
+		port = *portFlag
+	}
+	fmt.Println("Listening on port", port)
+
 	fmt.Println("Environment:")
 	for _, e := range os.Environ() {
 		fmt.Println(e)
 	}
 
+	u, _ := user.Current()
+	fmt.Println("User:", u.Name)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		//w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "Hello from %d", *port)
-		fmt.Printf("New request to %d\n", *port)
+		fmt.Fprintf(w, "Hello from %d", port)
+		fmt.Printf("New request to %d\n", port)
 	})
 
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
