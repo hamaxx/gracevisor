3a1443aac6cd1a378c41a190e93b156dfaf36bba
Domen Ipavec <domen@ipavec.net>
2015-07-10 10:04:41 +0200

    Added logging to stderr.
    Signed-off-by: Domen Ipavec <domen@ipavec.net>

--- demoapp/demoapp.go
+++ demoapp/demoapp.go
@@ -33,6 +33,8 @@ func main() {
 		fmt.Fprintf(w, "Hello from %d", port)
 		fmt.Printf("New request to %d\n", port)
 	})
+	
+	log.Println("This is stderr")
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
